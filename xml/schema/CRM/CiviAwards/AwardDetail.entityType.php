dc06c2b68845bdc4819550bf321f4b7809abd01d
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-10-22 16:05:45 +0100

    RSE-306: Generate Schema, DAO for the Aaward Manager and Award Details entities

---
+++ xml/schema/CRM/CiviAwards/AwardDetail.entityType.php
@@ -0,0 +1,11 @@
+<?php
+// This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
+return [
+  0 =>
+    [
+      'name' => 'AwardDetail',
+      'class' => 'CRM_CiviAwards_DAO_AwardDetail',
+      'table' => 'civicrm_award_detail',
+    ],
+];
