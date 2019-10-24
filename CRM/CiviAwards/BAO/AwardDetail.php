af7063c8b4d91f93e430c83fd1975347eaee5769
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-10-24 18:35:31 +0100

    RSE-306: Add the Award Manager and Award Detail BAO/Service.

--- CRM/CiviAwards/BAO/AwardDetail.php
+++ CRM/CiviAwards/BAO/AwardDetail.php
@@ -1,5 +1,7 @@
 <?php
 
+use CRM_CiviAwards_Service_AwardDetail as AwardDetailService;
+
 /**
  * Class CRM_CiviAwards_BAO_AwardDetail.
  */
@@ -17,14 +19,60 @@ class CRM_CiviAwards_BAO_AwardDetail extends CRM_CiviAwards_DAO_AwardDetail {
   public static function create(array $params) {
     $entityName = 'AwardDetail';
     $hook = empty($params['id']) ? 'create' : 'edit';
+    self::validateParams($params);
 
     CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
     $instance = new self();
     $instance->copyValues($params);
     $instance->save();
     CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
-
+    $instance->runAfterSave($hook, $params);
     return $instance;
+  }
+
+  /**
+   * Runs after save actions for the entity using the Award detail service.
+   *
+   * @param string $hook
+   *   Hook Value.
+   * @param array $params
+   *   Parameters.
+   */
+  private function runAfterSave($hook, array $params) {
+    $awardDetailService = new AwardDetailService();
+    if ($hook == 'create') {
+      $awardDetailService->postCreateActions($this, $params);
+    }
+    else {
+      $awardDetailService->postUpdateActions($this, $params);
+    }
+  }
+
+  /**
+   * Validation function.
+   *
+   * @param array $params
+   *   Parameters.
+   */
+  private static function validateParams(array $params) {
+    self::validateDates($params);
+  }
+
+  /**
+   * Ensures that the start and end dates are validated.
+   *
+   * @param array $params
+   *   Parameters.
+   */
+  private static function validateDates(array $params) {
+    if (!empty($params['start_date']) && !empty($params['end_date'])) {
+      $startDate = new DateTime($params['start_date']);
+      $endDate = new DateTime($params['end_date']);
+
+      if ($startDate > $endDate) {
+        throw new Exception("Award Start Date must not be greater than the End Date");
+      }
+    }
   }
 
 }
--- CRM/CiviAwards/BAO/AwardManager.php
+++ CRM/CiviAwards/BAO/AwardManager.php
@@ -15,12 +15,11 @@ class CRM_CiviAwards_BAO_AwardManager extends CRM_CiviAwards_DAO_AwardManager {
    *   AwardManager instance
    */
   public static function create(array $params) {
-    $className = 'CRM_CiviAwards_DAO_AwardManager';
     $entityName = 'AwardManager';
     $hook = empty($params['id']) ? 'create' : 'edit';
 
     CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
-    $instance = new $className();
+    $instance = new self();
     $instance->copyValues($params);
     $instance->save();
     CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
