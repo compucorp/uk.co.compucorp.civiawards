e97b74b457fc20818891a30b9e8471440ff8b02e
Debarshi Bhaumik <deb1990@gmail.com>
2019-11-29 07:04:37 +0000

    RSE-626: Stages UI changes

--- ang/civiawards/directives/award.directive.js
+++ ang/civiawards/directives/award.directive.js
@@ -12,7 +12,7 @@
     };
   });
 
-  module.controller('CiviAwardCreateEditAwardController', function ($scope, $window, crmApi, getSelect2Value, CaseStatus, AwardType, CaseTypeCategory) {
+  module.controller('CiviAwardCreateEditAwardController', function ($window, $scope, $location, crmApi, getSelect2Value, CaseTypeCategory) {
     var ts = CRM.ts('civicase');
 
     $scope.ts = ts;
@@ -21,7 +21,6 @@
     $scope.submitInProgress = false;
     $scope.awardTypeSelect2Options = [];
     $scope.awardDetailsID = null;
-    $scope.awardStages = CaseStatus.getAll();
     $scope.tabs = [
       { name: 'basicDetails', label: ts('Basic Details') },
       { name: 'stages', label: ts('Award Stages') }
@@ -30,8 +29,7 @@
       title: '',
       name: '',
       description: '',
-      isEnabled: true,
-      selectedAwardStages: {}
+      isEnabled: true
     };
     $scope.additionalDetails = {
       awardType: null,
@@ -40,79 +38,30 @@
       awardManagers: []
     };
 
-    $scope.saveAward = saveAward;
+    $scope.ifSaveButtonDisabled = ifSaveButtonDisabled;
     $scope.selectTab = selectTab;
+    $scope.saveAward = saveAward;
+    $scope.saveNewAward = saveNewAward;
+    $scope.saveAndNavigateToDashboard = saveAndNavigateToDashboard;
+    $scope.navigateToDashboard = navigateToDashboard;
 
     (function init () {
-      $scope.activeTab = $scope.tabs[0].name;
-      $scope.awardTypeSelect2Options = getAwardTypeSelect2Options();
-
-      $scope.$watch('basicDetails.title', titleWatcher);
-
       if ($scope.awardId) {
+        $scope.activeTab = $scope.tabs[1].name;
         fetchAwardInformation()
           .then(function (result) {
-            setBasicDetails(result.caseType);
-            setAdditionalInformation(result.additionalDetails);
+            $scope.$emit('civiawards::edit-award::details-fetched', result);
           });
-      } else {
-        enableAllAwardStages();
       }
     }());
 
     /**
-     * Watcher for Title form field
-     */
-    function titleWatcher () {
-      if (!$scope.awardId && $scope.basic_details_form.awardName.$pristine) {
-        $scope.basicDetails.name = $scope.basicDetails.title.replace(/ /g, '_').replace(/[^a-zA-Z0-9_]/g, '').toLowerCase();
-      }
-    }
-
-    /**
-     * Set basic details
+     * Selects a tab as active
      *
-     * @param {object} caseType case type
+     * @param {string} tab tab name
      */
-    function setBasicDetails (caseType) {
-      $scope.pageTitle = caseType.title;
-      $scope.basicDetails.title = caseType.title;
-      $scope.basicDetails.name = caseType.name;
-      $scope.basicDetails.description = caseType.description;
-      $scope.basicDetails.isEnabled = caseType.is_active === '1';
-      $scope.basicDetails.selectedAwardStages = getSelectedAwardStages(caseType.definition.statuses);
-    }
-
-    /**
-     * Get Selected Award stages
-     *
-     * @param {Array} awardStages award stages fetched from api
-     * @returns {object} selected award stages
-     */
-    function getSelectedAwardStages (awardStages) {
-      var selectedAwardStages = {};
-
-      _.each(awardStages, function (stageName) {
-        var awardStage = _.find($scope.awardStages, function (stage) {
-          return stage.name === stageName;
-        });
-        selectedAwardStages[awardStage.value] = true;
-      });
-
-      return selectedAwardStages;
-    }
-
-    /**
-     * Set Additional Details of award
-     *
-     * @param {object} additionalDetails additional details
-     */
-    function setAdditionalInformation (additionalDetails) {
-      $scope.awardDetailsID = additionalDetails.id;
-      $scope.additionalDetails.startDate = additionalDetails.start_date;
-      $scope.additionalDetails.endDate = additionalDetails.end_date;
-      $scope.additionalDetails.awardType = additionalDetails.award_type;
-      $scope.additionalDetails.awardManagers = additionalDetails.award_manager.join();
+    function selectTab (tab) {
+      $scope.activeTab = tab;
     }
 
     /**
@@ -128,49 +77,82 @@
     }
 
     /**
-     * Returns Award Types to be used in the UI
+     * Check if Save button should be disabled
      *
-     * @returns {Array} award types array in a format suitable for select 2
+     * @returns {boolean} if Save button should be disabled
      */
-    function getAwardTypeSelect2Options () {
-      return _.map(AwardType.getAll(), function (awardType) {
-        return { id: awardType.value, text: awardType.label, name: awardType.name };
-      });
-    }
-    /**
-     * Selects a tab as active
-     *
-     * @param {string} tab tab name
-     */
-    function selectTab (tab) {
-      $scope.activeTab = tab;
+    function ifSaveButtonDisabled () {
+      return !$scope.basic_details_form.$valid || $scope.submitInProgress;
     }
 
     /**
-     * Enable All Award Stages
+     * Generate Award `name` from the sent text
+     *
+     * @param {string} text string from which name would be generated
+     * @returns {string} name of the award
      */
-    function enableAllAwardStages () {
-      _.each($scope.awardStages, function (awardStage) {
-        $scope.basicDetails.selectedAwardStages[awardStage.value] = true;
-      });
+    function generateAwardName (text) {
+      return text
+        .replace(/ /g, '_')
+        .replace(/[^a-zA-Z0-9_]/g, '')
+        .toLowerCase();
+    }
+
+    /**
+     * Saves a New Award
+     */
+    function saveNewAward () {
+      saveAward()
+        .then(navigateToAwardEditPage);
+    }
+
+    /**
+     * Saves an existing Award and navigates to dashboard
+     */
+    function saveAndNavigateToDashboard () {
+      saveAward()
+        .then(navigateToDashboard);
     }
 
     /**
      * Save Award
+     *
+     * @returns {Promise} promise
      */
     function saveAward () {
       $scope.submitInProgress = true;
-      saveCaseTypeBasicDetails()
+
+      return saveCaseTypeBasicDetails()
         .then(saveAdditionAwardDetails)
-        .then(function () {
-          $window.location.href = '/civicrm/case/a/?case_type_category=awards#/case?case_type_category=awards';
+        .then(function (award) {
+          return award.case_type_id;
         })
         .catch(function (error) {
-          CRM.alert(error.error_message, ts('Error'), 'error');
+          if (error.error_code === 'already exists') {
+            CRM.alert('Duplicate Award Title. Please choose and different title and try again.', ts('Error'), 'error');
+          } else {
+            CRM.alert(error.error_message, ts('Error'), 'error');
+          }
         })
         .finally(function () {
           $scope.submitInProgress = false;
         });
+    }
+
+    /**
+     * Navigate to the Dashboard Page
+     */
+    function navigateToDashboard () {
+      $window.location.href = '/civicrm/case/a/?case_type_category=awards#/case?case_type_category=awards';
+    }
+
+    /**
+     * Navigate to edit the given award
+     *
+     * @param {string/number} awardID id of the award
+     */
+    function navigateToAwardEditPage (awardID) {
+      $location.path('/awards/' + awardID);
     }
 
     /**
@@ -179,24 +161,16 @@
      * @returns {Promise} promise
      */
     function saveCaseTypeBasicDetails () {
-      var selectedAwardStageNames = _.chain($scope.awardStages)
-        .filter(function (stage) {
-          return $scope.basicDetails.selectedAwardStages[stage.value];
-        })
-        .map(function (stage) { return stage.name; })
-        .value();
-      var areAllAwardsSelected = selectedAwardStageNames.length === _.size($scope.awardStages);
       var params = {
         sequential: true,
         title: $scope.basicDetails.title,
         description: $scope.basicDetails.description,
         is_active: $scope.basicDetails.isEnabled,
         case_type_category: CaseTypeCategory.findByName('awards').value,
-        name: $scope.basicDetails.name,
-        definition: {
-          statuses: areAllAwardsSelected ? [] : selectedAwardStageNames
-        }
+        name: generateAwardName($scope.basicDetails.title)
       };
+
+      prepareAwardStagesWhenEditings(params);
 
       if ($scope.awardId) {
         params.id = $scope.awardId;
@@ -208,13 +182,37 @@
     }
 
     /**
+     * Prepare the Award Stages paramaters when editing an award
+     *
+     * @param {object} params parameters
+     */
+    function prepareAwardStagesWhenEditings (params) {
+      if (!$scope.awardId) {
+        return;
+      }
+
+      var selectedAwardStageNames = _.chain($scope.awardStages)
+        .filter(function (stage) {
+          return $scope.basicDetails.selectedAwardStages[stage.value];
+        })
+        .map(function (stage) { return stage.name; })
+        .value();
+      var areAllAwardsSelected = selectedAwardStageNames.length === _.size($scope.awardStages);
+
+      params.definition = {
+        statuses: areAllAwardsSelected ? [] : selectedAwardStageNames
+      };
+    }
+
+    /**
      * Save Additional Award Details, which are saved using AwardDetail entity
      *
      * @param {object} award award object
      * @returns {Promise} promise
      */
     function saveAdditionAwardDetails (award) {
-      var additionalAwardDetails = {
+      var params = {
+        sequential: true,
         award_manager: getSelect2Value($scope.additionalDetails.awardManagers),
         case_type_id: award.id,
         start_date: $scope.additionalDetails.startDate,
@@ -223,10 +221,13 @@
       };
 
       if ($scope.awardDetailsID) {
-        additionalAwardDetails.id = $scope.awardDetailsID;
+        params.id = $scope.awardDetailsID;
       }
 
-      return crmApi('AwardDetail', 'create', additionalAwardDetails);
+      return crmApi('AwardDetail', 'create', params)
+        .then(function (awardData) {
+          return awardData.values[0];
+        });
     }
   });
 })(angular, CRM.$, CRM._);
---
+++ ang/civiawards/directives/basic-details-form.directive.js
@@ -0,0 +1,69 @@
+(function (angular, $, _) {
+  var module = angular.module('civiawards');
+
+  module.directive('civiawardBasicDetailsForm', function () {
+    return {
+      controller: 'CiviawardBasicDetailsFormController',
+      templateUrl: '~/civiawards/directives/basic-details-form.directive.html',
+      restrict: 'E'
+    };
+  });
+
+  module.controller('CiviawardBasicDetailsFormController', function ($rootScope, $scope, AwardType) {
+    var ts = CRM.ts('civicase');
+    $scope.ts = ts;
+
+    (function init () {
+      $scope.awardTypeSelect2Options = getAwardTypeSelect2Options();
+      $rootScope.$on('civiawards::edit-award::details-fetched', setDetails);
+    }());
+
+    /**
+     * Set Details
+     *
+     * @param {*} e event
+     * @param {object} details details of the award
+     */
+    function setDetails (e, details) {
+      setBasicDetails(details.caseType);
+      setAdditionalInformation(details.additionalDetails);
+    }
+
+    /**
+     * Set basic details
+     *
+     * @param {object} caseType case type
+     */
+    function setBasicDetails (caseType) {
+      $scope.pageTitle = 'Configure Award - ' + caseType.title;
+      $scope.basicDetails.title = caseType.title;
+      $scope.basicDetails.name = caseType.name;
+      $scope.basicDetails.description = caseType.description;
+      $scope.basicDetails.isEnabled = caseType.is_active === '1';
+    }
+
+    /**
+     * Set Additional Details of award
+     *
+     * @param {object} additionalDetails additional details
+     */
+    function setAdditionalInformation (additionalDetails) {
+      $scope.awardDetailsID = additionalDetails.id;
+      $scope.additionalDetails.startDate = additionalDetails.start_date;
+      $scope.additionalDetails.endDate = additionalDetails.end_date;
+      $scope.additionalDetails.awardType = additionalDetails.award_type;
+      $scope.additionalDetails.awardManagers = additionalDetails.award_manager.join();
+    }
+
+    /**
+     * Returns Award Types to be used in the UI
+     *
+     * @returns {Array} award types array in a format suitable for select 2
+     */
+    function getAwardTypeSelect2Options () {
+      return _.map(AwardType.getAll(), function (awardType) {
+        return { id: awardType.value, text: awardType.label, name: awardType.name };
+      });
+    }
+  });
+})(angular, CRM.$, CRM._);
