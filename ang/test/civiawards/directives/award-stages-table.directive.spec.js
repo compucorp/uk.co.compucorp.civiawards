c7ead9c7cc0053ca205ba7787458e485e94c56fb
Debarshi Bhaumik <deb1990@gmail.com>
2019-12-03 06:55:20 +0000

    RSE-626: Unit Tests

---
+++ ang/test/civiawards/directives/award-stages-table.directive.spec.js
@@ -0,0 +1,65 @@
+/* eslint-env jasmine */
+(function (_) {
+  describe('civiawardAwardStagesTable', () => {
+    var $rootScope, $controller, $scope, CaseStatus, AwardMockData;
+
+    beforeEach(module('civiawards.templates', 'civiawards', 'civicase.data', 'civiawards.data'));
+
+    beforeEach(inject((_$controller_, _$rootScope_, _CaseStatus_, _AwardMockData_) => {
+      $controller = _$controller_;
+      $rootScope = _$rootScope_;
+      CaseStatus = _CaseStatus_;
+      AwardMockData = _AwardMockData_;
+      $scope = $rootScope.$new();
+
+      $scope.$digest();
+    }));
+
+    describe('basic tests', () => {
+      beforeEach(() => {
+        createController();
+      });
+
+      it('displays all award stages', () => {
+        expect($scope.awardStages).toEqual(CaseStatus.getAll());
+      });
+    });
+
+    describe('when award checkbox is clicked', () => {
+      beforeEach(() => {
+        createController();
+        $scope.basicDetails = { selectedAwardStages: { 1: true } };
+
+        $scope.toggleAwardStage({ value: '1' });
+      });
+
+      it('toggles the clicked award stage', () => {
+        expect($scope.basicDetails.selectedAwardStages[1]).toEqual(false);
+      });
+    });
+
+    describe('when an existing awards details are fetched', () => {
+      beforeEach(() => {
+        createController();
+        $scope.basicDetails = {};
+
+        $scope.$emit('civiawards::edit-award::details-fetched', {
+          caseType: AwardMockData
+        });
+      });
+
+      it('shows are saved stages', () => {
+        expect($scope.basicDetails.selectedAwardStages).toEqual({ 1: true, 2: true, 3: true });
+      });
+    });
+
+    /**
+     * Create Controller
+     */
+    function createController () {
+      $controller('CiviawardAwardStagesTableController', {
+        $scope: $scope
+      });
+    }
+  });
+}(CRM._));
--- ang/test/civiawards/directives/award.directive.spec.js
+++ ang/test/civiawards/directives/award.directive.spec.js
@@ -1,18 +1,20 @@
 /* eslint-env jasmine */
 (function (_) {
   describe('civiaward', () => {
-    var $q, $controller, $rootScope, $scope, $window, crmApi,
+    var $q, $controller, $rootScope, $scope, $window, $location, crmApi,
       CaseStatus, AwardMockData, AwardAdditionalDetailsMockData;
 
     beforeEach(module('civicase-base', 'civiawards.templates', 'civiawards', 'civicase.data', 'civiawards.data', ($provide) => {
       $provide.value('crmApi', jasmine.createSpy('crmApi'));
       $provide.value('$window', { location: {} });
+      $provide.value('$location', jasmine.createSpyObj('$location', ['path']));
     }));
 
-    beforeEach(inject((_$q_, _$controller_, _$window_, _$rootScope_, _crmApi_,
+    beforeEach(inject((_$q_, _$controller_, _$window_, _$location_, _$rootScope_, _crmApi_,
       _CaseStatus_, _AwardMockData_, _AwardAdditionalDetailsMockData_) => {
       $q = _$q_;
       $window = _$window_;
+      $location = _$location_;
       $controller = _$controller_;
       $rootScope = _$rootScope_;
       crmApi = _crmApi_;
@@ -32,16 +34,8 @@
           createController({ ifNewAward: true });
         });
 
-        it('disables the name field', () => {
-          expect($scope.isNameDisabled).toBe(true);
-        });
-
         it('disables the submit button', () => {
           expect($scope.submitInProgress).toBe(false);
-        });
-
-        it('shows all award stages', () => {
-          expect($scope.awardStages).toBe(CaseStatus.getAll());
         });
 
         describe('all basic fields', () => {
@@ -58,8 +52,7 @@
               title: '',
               name: '',
               description: '',
-              isEnabled: true,
-              selectedAwardStages: expectedSelectedAwardStages
+              isEnabled: true
             });
           });
 
@@ -72,31 +65,6 @@
             });
           });
         });
-
-        describe('when the title changes', () => {
-          beforeEach(() => {
-            $scope.basicDetails.title = 'Some custom text';
-            $scope.$digest();
-          });
-
-          it('updating the title changes the name field too', () => {
-            expect($scope.basicDetails.name).toBe('some_custom_text');
-          });
-        });
-
-        describe('if the name is manually changed', () => {
-          beforeEach(() => {
-            $scope.basicDetails.title = 'Some custom text';
-            $scope.$digest();
-            $scope.basic_details_form.awardName.$pristine = false;
-            $scope.basicDetails.title = 'another custom text';
-            $scope.$digest();
-          });
-
-          it('does not changes the name field when editing title', () => {
-            expect($scope.basicDetails.name).toBe('some_custom_text');
-          });
-        });
       });
 
       describe('when editing existing award', function () {
@@ -106,42 +74,105 @@
             additionalDetails: AwardAdditionalDetailsMockData
           }));
 
+          spyOn($scope, '$emit');
           createController({ ifNewAward: false });
           $scope.$digest();
         });
 
-        it('fetches the award information', () => {
-          expect(crmApi).toHaveBeenCalledWith({
-            caseType: ['CaseType', 'getsingle', { sequential: true, id: '10' }],
-            additionalDetails: ['AwardDetail', 'getsingle', { sequential: true, case_type_id: '10' }]
+        it('fires an event with the award details', () => {
+          expect($scope.$emit).toHaveBeenCalledWith('civiawards::edit-award::details-fetched', {
+            caseType: {
+              id: '10',
+              name: 'new_award',
+              title: 'New Award',
+              description: 'Description',
+              is_active: '1',
+              weight: '1',
+              definition: {
+                statuses: ['Open', 'Closed', 'Urgent']
+              },
+              case_type_category: '4',
+              is_forkable: '1',
+              is_forked: ''
+            },
+            additionalDetails: {
+              id: '1',
+              case_type_id: '10',
+              award_type: '1',
+              start_date: '2019-10-29',
+              end_date: '2019-11-29',
+              award_manager: ['2', '1']
+            }
           });
         });
+      });
+    });
 
-        it('displays the basic awards information', () => {
-          expect($scope.basicDetails.title).toBe(AwardMockData.title);
-          expect($scope.basicDetails.name).toBe(AwardMockData.name);
-          expect($scope.basicDetails.description).toBe(AwardMockData.description);
-          expect($scope.basicDetails.isEnabled).toBe(AwardMockData.is_active === '1');
+    describe('save button disability', () => {
+      var returnValue;
+
+      beforeEach(() => {
+        createController({ ifNewAward: true });
+      });
+
+      describe('when the basic details form is valid and submit is in progress', () => {
+        beforeEach(() => {
+          $scope.basic_details_form.$valid = true;
+          $scope.submitInProgress = true;
+          returnValue = $scope.ifSaveButtonDisabled();
         });
 
-        it('displays the additional awards information', () => {
-          expect($scope.awardDetailsID).toBe(AwardAdditionalDetailsMockData.id);
-          expect($scope.additionalDetails.startDate).toBe(AwardAdditionalDetailsMockData.start_date);
-          expect($scope.additionalDetails.endDate).toBe(AwardAdditionalDetailsMockData.end_date);
-          expect($scope.additionalDetails.awardType).toBe(AwardAdditionalDetailsMockData.award_type);
-          expect($scope.additionalDetails.awardManagers).toBe(AwardAdditionalDetailsMockData.award_manager.toString());
+        it('disables the save button', () => {
+          expect(returnValue).toBe(true);
+        });
+      });
+
+      describe('when the basic details form is valid and submit is not in progress', () => {
+        beforeEach(() => {
+          $scope.basic_details_form.$valid = true;
+          $scope.submitInProgress = false;
+          returnValue = $scope.ifSaveButtonDisabled();
         });
 
-        describe('name field', () => {
-          beforeEach(() => {
-            $scope.basicDetails.title = 'Some custom text';
-            $scope.$digest();
-          });
-
-          it('updating the title does not change the name field', () => {
-            expect($scope.basicDetails.name).toBe('new_award');
-          });
+        it('enables the save button', () => {
+          expect(returnValue).toBe(false);
         });
+      });
+
+      describe('when the basic details form is invalid and submit is in progress', () => {
+        beforeEach(() => {
+          $scope.basic_details_form.$valid = false;
+          $scope.submitInProgress = true;
+          returnValue = $scope.ifSaveButtonDisabled();
+        });
+
+        it('disables the save button', () => {
+          expect(returnValue).toBe(true);
+        });
+      });
+
+      describe('when the basic details form is invalid and submit is not in progress', () => {
+        beforeEach(() => {
+          $scope.basic_details_form.$valid = false;
+          $scope.submitInProgress = false;
+          returnValue = $scope.ifSaveButtonDisabled();
+        });
+
+        it('disables the save button', () => {
+          expect(returnValue).toBe(true);
+        });
+      });
+    });
+
+    describe('select tab', () => {
+      beforeEach(() => {
+        createController({ ifNewAward: true });
+
+        $scope.selectTab('basicDetails');
+      });
+
+      it('selects the clicked tab', () => {
+        expect($scope.activeTab).toBe('basicDetails');
       });
     });
 
@@ -149,18 +180,8 @@
       describe('when creating a new award', () => {
         beforeEach(() => {
           createController({ ifNewAward: true });
-          $scope.basicDetails.title = 'title';
-          $scope.basicDetails.description = 'description';
-          $scope.basicDetails.isEnabled = true;
-          $scope.basicDetails.selectedAwardStages = { 1: true };
-          $scope.additionalDetails.awardManagers = '2,1';
-          $scope.additionalDetails.startDate = AwardAdditionalDetailsMockData.start_date;
-          $scope.additionalDetails.endDate = AwardAdditionalDetailsMockData.end_date;
-          $scope.additionalDetails.awardType = AwardAdditionalDetailsMockData.award_type;
+          setAwardDetails();
 
-          crmApi.and.returnValue($q.resolve({
-            values: [AwardAdditionalDetailsMockData]
-          }));
           $scope.saveAward();
         });
 
@@ -180,13 +201,13 @@
               description: 'description',
               is_active: true,
               case_type_category: '3',
-              name: '',
-              definition: { statuses: ['Open'] }
+              name: 'title'
             });
           });
 
           it('saves the additional award details', () => {
             expect(crmApi).toHaveBeenCalledWith('AwardDetail', 'create', {
+              sequential: true,
               case_type_id: '1',
               start_date: AwardAdditionalDetailsMockData.start_date,
               end_date: AwardAdditionalDetailsMockData.end_date,
@@ -195,16 +216,85 @@
             });
           });
 
-          it('redirects to dashboard page', () => {
-            expect($window.location.href).toBe('/civicrm/case/a/?case_type_category=awards#/case?case_type_category=awards');
-          });
-
           it('enables the save button after api has responded', () => {
             expect($scope.submitInProgress).toBe(false);
           });
         });
       });
+
+      describe('when editing an award', () => {
+        beforeEach(() => {
+          createController({ ifNewAward: false });
+          $scope.awardStages = CaseStatus.getAll();
+          $scope.basicDetails.selectedAwardStages = { 1: true };
+          setAwardDetails();
+
+          crmApi.and.returnValue($q.resolve({
+            values: [AwardAdditionalDetailsMockData]
+          }));
+          $scope.saveAward();
+          $scope.$digest();
+        });
+
+        it('saves the basic award details', () => {
+          expect(crmApi).toHaveBeenCalledWith('CaseType', 'create', {
+            sequential: true,
+            title: 'title',
+            description: 'description',
+            is_active: true,
+            case_type_category: '3',
+            name: 'title',
+            definition: { statuses: ['Open'] },
+            id: '10'
+          });
+        });
+      });
     });
+
+    describe('when saving a new award', () => {
+      beforeEach(() => {
+        createController({ ifNewAward: true });
+        setAwardDetails();
+
+        $scope.saveNewAward();
+        $scope.$digest();
+      });
+
+      it('redirects to edit the award', () => {
+        expect($location.path).toHaveBeenCalledWith('/awards/10');
+      });
+    });
+
+    describe('when SAVE AND DONE is clicked', () => {
+      beforeEach(() => {
+        createController({ ifNewAward: true });
+        setAwardDetails();
+
+        $scope.saveAndNavigateToDashboard();
+        $scope.$digest();
+      });
+
+      it('redirects to award dashboard page', () => {
+        expect($window.location.href).toBe('/civicrm/case/a/?case_type_category=awards#/case?case_type_category=awards');
+      });
+    });
+
+    /**
+     * Set Award Details
+     */
+    function setAwardDetails () {
+      $scope.basicDetails.title = 'title';
+      $scope.basicDetails.description = 'description';
+      $scope.basicDetails.isEnabled = true;
+      $scope.additionalDetails.awardManagers = '2,1';
+      $scope.additionalDetails.startDate = AwardAdditionalDetailsMockData.start_date;
+      $scope.additionalDetails.endDate = AwardAdditionalDetailsMockData.end_date;
+      $scope.additionalDetails.awardType = AwardAdditionalDetailsMockData.award_type;
+
+      crmApi.and.returnValue($q.resolve({
+        values: [AwardAdditionalDetailsMockData]
+      }));
+    }
 
     /**
      * Create Controller
