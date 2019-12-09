9883592b9db5145ae7dd8706f736afa0467e52a4
Rene Olivo <rene.olivo.do@gmail.com>
2019-12-09 19:57:35 +0000

    RSE-366: Provide Add Award dashboard action button

---
+++ ang/civiawards/dashboard/configs/awards-dashboard-action-buttons.config.js
@@ -0,0 +1,16 @@
+(function (angular) {
+  var module = angular.module('civiawards');
+
+  module.config(function (DashboardActionButtonsProvider) {
+    var awardsActionButtons = [
+      {
+        buttonClass: 'btn btn-primary civicase__dashboard__action-btn civicase__dashboard__action-btn--white',
+        iconClass: 'add_circle',
+        identifier: 'AddAward',
+        label: 'Create new award'
+      }
+    ];
+
+    DashboardActionButtonsProvider.addButtons(awardsActionButtons);
+  });
+})(angular);
---
+++ ang/test/civiawards/dashboard/services/add-award-dashboard-action-button.service.spec.js
@@ -0,0 +1,53 @@
+/* eslint-env jasmine */
+
+(function (_) {
+  describe('Add Award Dashboard Action Button', () => {
+    let $location, AddAwardDashboardActionButton;
+
+    beforeEach(module('civiawards'));
+
+    beforeEach(inject((_$location_, _AddAwardDashboardActionButton_) => {
+      $location = _$location_;
+      AddAwardDashboardActionButton = _AddAwardDashboardActionButton_;
+    }));
+
+    describe('button visibility', () => {
+      var isButtonVisible;
+
+      describe('when viewing the awards dashboard', () => {
+        beforeEach(() => {
+          $location.search('case_type_category', 'awards');
+
+          isButtonVisible = AddAwardDashboardActionButton.isVisible();
+        });
+
+        it('displays the add award button', () => {
+          expect(isButtonVisible).toBe(true);
+        });
+      });
+
+      describe('when viewing any other dashboard', () => {
+        beforeEach(() => {
+          $location.search('case_type_category', 'cases');
+
+          isButtonVisible = AddAwardDashboardActionButton.isVisible();
+        });
+
+        it('does not display the add award button', () => {
+          expect(isButtonVisible).toBe(false);
+        });
+      });
+    });
+
+    describe('when clicking the action button', () => {
+      beforeEach(() => {
+        spyOn($location, 'url');
+        AddAwardDashboardActionButton.clickHandler();
+      });
+
+      it('redirects the user to the create award screen', () => {
+        expect($location.url).toHaveBeenCalledWith('awards/new');
+      });
+    });
+  });
+})(CRM._);
