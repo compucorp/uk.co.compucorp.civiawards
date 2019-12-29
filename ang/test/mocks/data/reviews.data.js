0192c67e7583af62d8c054bf400d7005180bda23
reneolivo <minet.ws@gmail.com>
2019-12-29 17:36:31 -0400

    RSE-359: Add reviews and applications mock data

---
+++ ang/test/mocks/data/reviews.data.js
@@ -0,0 +1,66 @@
+(() => {
+  var module = angular.module('civiawards.data');
+
+  module.constant('ReviewActivitiesMockData', [
+    {
+      id: '128',
+      activity_type_id: '69',
+      activity_date_time: '2019-12-23 08:19:09',
+      status_id: '2',
+      priority_id: '2',
+      is_test: '0',
+      is_auto: '0',
+      is_current_revision: '1',
+      is_deleted: '0',
+      is_star: '0',
+      created_date: '2019-12-23 08:19:09',
+      modified_date: '2019-12-23 08:19:09',
+      custom_19: '80',
+      custom_20: '50',
+      custom_21: '19',
+      source_contact_id: '2',
+      source_contact_name: 'Compu Admin',
+      source_contact_sort_name: 'Admin, Compu'
+    },
+    {
+      id: '129',
+      activity_type_id: '69',
+      activity_date_time: '2019-12-23 08:20:06',
+      status_id: '2',
+      priority_id: '2',
+      is_test: '0',
+      is_auto: '0',
+      is_current_revision: '1',
+      is_deleted: '0',
+      is_star: '0',
+      created_date: '2019-12-23 08:20:06',
+      modified_date: '2019-12-23 08:20:06',
+      custom_19: '81',
+      custom_20: '51',
+      custom_21: '20',
+      source_contact_id: '2',
+      source_contact_name: 'Compu Admin',
+      source_contact_sort_name: 'Admin, Compu'
+    },
+    {
+      id: '132',
+      activity_type_id: '69',
+      activity_date_time: '2019-12-23 08:56:00',
+      status_id: '2',
+      priority_id: '2',
+      is_test: '0',
+      is_auto: '0',
+      is_current_revision: '1',
+      is_deleted: '0',
+      is_star: '0',
+      created_date: '2019-12-23 08:56:00',
+      modified_date: '2019-12-23 08:56:00',
+      custom_19: '82',
+      custom_20: '52',
+      custom_21: '21',
+      source_contact_id: '2',
+      source_contact_name: 'Compu Admin',
+      source_contact_sort_name: 'Admin, Compu'
+    }
+  ]);
+})();
