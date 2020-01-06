0fb566c032afa9e0c1fb3c34d9bf386fc64374f9
Rene Olivo <rene.olivo.do@gmail.com>
2020-01-06 03:13:40 +0000

    RSE-657: Check permissions when editing awards

--- ang/civiawards/shared/configs/case-type-buttons.config.js
+++ ang/civiawards/shared/configs/case-type-buttons.config.js
@@ -1,29 +1,35 @@
-(function (angular, _, url) {
+(function (angular, _, checkPermission, url) {
   var module = angular.module('civiawards');
   var AWARDS_CATEGORY_NAME = 'awards';
   var AWARD_CONFIG_URL = 'civicrm/a/#/awards/';
+  var EDIT_AWARD_PERMISSION = 'create/edit awards';
 
   module.config(function (CaseTypeProvider, CaseTypeCategoryProvider, DashboardCaseTypeButtonsProvider) {
     var awardCategory = CaseTypeCategoryProvider.findByName(AWARDS_CATEGORY_NAME);
     var awardCaseTypes = CaseTypeProvider.getByCategory(awardCategory.value);
+    var canEditAwards = checkPermission(EDIT_AWARD_PERMISSION);
 
-    addConfigurationButtonsToCaseTypes(awardCaseTypes);
+    (function () {
+      if (!canEditAwards) {
+        return;
+      }
+
+      _.forEach(awardCaseTypes, addConfigurationButtonsToCaseType);
+    })();
 
     /**
-     * Adds configuration buttons to the given case types. The buttons url point
+     * Adds configuration buttons to the given case type. The buttons url point
      * towards the awards configuration form.
      *
-     * @param {object[]} caseTypes a list of case types objects.
+     * @param {object} caseType case type data.
      */
-    function addConfigurationButtonsToCaseTypes (caseTypes) {
-      _.forEach(caseTypes, function (caseType) {
-        var caseTypeConfigUrl = url(AWARD_CONFIG_URL + caseType.id);
+    function addConfigurationButtonsToCaseType (caseType) {
+      var caseTypeConfigUrl = url(AWARD_CONFIG_URL + caseType.id);
 
-        DashboardCaseTypeButtonsProvider.addButtons(caseType.name, [{
-          icon: 'fa fa-cog',
-          url: caseTypeConfigUrl
-        }]);
-      });
+      DashboardCaseTypeButtonsProvider.addButtons(caseType.name, [{
+        icon: 'fa fa-cog',
+        url: caseTypeConfigUrl
+      }]);
     }
   });
-})(angular, CRM._, CRM.url);
+})(angular, CRM._, CRM.checkPerm, CRM.url);
--- ang/test/civiawards/shared/configs/case-type-buttons.config.spec.js
+++ ang/test/civiawards/shared/configs/case-type-buttons.config.spec.js
@@ -9,40 +9,75 @@
 
     beforeEach(module('civiawards.data', 'civicase-base', 'civiawards'));
 
-    beforeEach(inject((_AwardMockData_, _DashboardCaseTypeButtons_, caseTypeCategoriesMockData, _CaseTypesMockData_) => {
-      AwardMockData = _AwardMockData_;
-      CaseTypesMockData = _CaseTypesMockData_.get();
-      DashboardCaseTypeButtons = _DashboardCaseTypeButtons_;
-      AwardsCategory = _.find(
-        caseTypeCategoriesMockData,
-        (category) => category.name === AWARDS_CATEGORY_NAME
-      );
-    }));
+    describe('when the user can edit awards', () => {
+      beforeEach(() => {
+        CRM.checkPerm.and.returnValue(true);
 
-    describe('after the awards module has been configured', () => {
-      it('it adds the configuration url to the awards case type', () => {
-        expect(DashboardCaseTypeButtons).toEqual({
-          [AwardMockData.name]: [{
-            icon: 'fa fa-cog',
-            url: getCrmUrl(AWARD_CONFIG_URL + AwardMockData.id)
-          }]
+        injectDependencies();
+      });
+
+      describe('after the awards module has been configured', () => {
+        it('checks that the user can edit awards', () => {
+          expect(CRM.checkPerm).toHaveBeenCalledWith('create/edit awards');
+        });
+
+        it('adds the configuration url to the awards case type', () => {
+          expect(DashboardCaseTypeButtons).toEqual({
+            [AwardMockData.name]: [{
+              icon: 'fa fa-cog',
+              url: getCrmUrl(AWARD_CONFIG_URL + AwardMockData.id)
+            }]
+          });
+        });
+      });
+
+      describe('when requesting the buttons for non award case types', () => {
+        let nonAwardscaseTypes;
+
+        beforeEach(() => {
+          nonAwardscaseTypes = _.chain(CaseTypesMockData)
+            .filter(caseType => caseType.category !== AwardsCategory.value)
+            .map('name')
+            .value();
+        });
+
+        it('does not add configuration buttons to non award case types', () => {
+          expect(_.keys(DashboardCaseTypeButtons)).not.toContain(nonAwardscaseTypes);
         });
       });
     });
 
-    describe('when requesting the buttons for non award case types', () => {
-      let nonAwardscaseTypes;
-
+    describe('when the user cannot edit awards', () => {
       beforeEach(() => {
-        nonAwardscaseTypes = _.chain(CaseTypesMockData)
-          .filter(caseType => caseType.category !== AwardsCategory.value)
-          .map('name')
-          .value();
+        CRM.checkPerm.and.returnValue(false);
+
+        injectDependencies();
       });
 
-      it('it does not add configuration buttons to non award case types', () => {
-        expect(_.keys(DashboardCaseTypeButtons)).not.toContain(nonAwardscaseTypes);
+      describe('after the awards module has been configured', () => {
+        it('checks that the user can edit awards', () => {
+          expect(CRM.checkPerm).toHaveBeenCalledWith('create/edit awards');
+        });
+
+        it('does not add the configuration url to the awards case type', () => {
+          expect(DashboardCaseTypeButtons).toEqual({});
+        });
       });
     });
+
+    /**
+     * Injects and hoists the dependencies needed for the test.
+     */
+    function injectDependencies () {
+      inject((_AwardMockData_, _DashboardCaseTypeButtons_, caseTypeCategoriesMockData, _CaseTypesMockData_) => {
+        AwardMockData = _AwardMockData_;
+        CaseTypesMockData = _CaseTypesMockData_.get();
+        DashboardCaseTypeButtons = _DashboardCaseTypeButtons_;
+        AwardsCategory = _.find(
+          caseTypeCategoriesMockData,
+          (category) => category.name === AWARDS_CATEGORY_NAME
+        );
+      });
+    }
   });
 })(CRM._, angular, CRM.url);
