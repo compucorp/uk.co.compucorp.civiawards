ca9323cb8c95bfb5649dbbcd4be400ba31e6ff8e
Debarshi Bhaumik <deb1990@gmail.com>
2020-02-14 10:26:47 +0000

    RSE-751: Change Award to Application

--- config/word_replacement/awards_category.php
+++ config/word_replacement/awards_category.php
@@ -4,9 +4,10 @@
  * @file
  * Returns the words to be replaced and the replacement words.
  */
+
 return [
-  'Case' => 'Award',
-  'Cases' => 'Awards',
-  'case' => 'award',
-  'cases' => 'awards',
+  'Case' => 'Application',
+  'Cases' => 'Applications',
+  'case' => 'application',
+  'cases' => 'applications',
 ];
