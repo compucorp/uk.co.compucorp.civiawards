cbd469c66865b68304c9ab36f4fc08da74bdc4bb
Debarshi Bhaumik <deb1990@gmail.com>
2020-03-03 05:26:04 +0000

    RSE-678: Review Panel group selection ui + functionality

--- ang/civiawards/award-creation/directives/review-panels.directive.js
+++ ang/civiawards/award-creation/directives/review-panels.directive.js
@@ -13,11 +13,86 @@
   });
 
   module.controller('CiviawardReviewPanelsController', function (
-    $scope, ts, dialogService, crmApi, crmStatus) {
-    $scope.reviewPanel = {};
+    $scope, ts, dialogService, crmApi, crmStatus, getSelect2Value) {
     $scope.submitInProgress = false;
+    $scope.relationshipTypes = [];
+    $scope.reviewPanel = {
+      groups: { include: [], exclude: [] },
+      isEnabled: false,
+      title: '',
+      relationships: [{
+        contacts: '',
+        type: ''
+      }]
+    };
 
     $scope.openCreateReviewPanelPopup = openCreateReviewPanelPopup;
+    $scope.addMoreRelations = addMoreRelations;
+    $scope.removeRelation = removeRelation;
+
+    (function init () {
+      getRelationshipsTypes()
+        .then(function (relationshipTypeData) {
+          $scope.relationshipTypes = prepareRelationshipsTypes(relationshipTypeData.values);
+        });
+    }());
+
+    /**
+     * Add More Relations
+     */
+    function addMoreRelations () {
+      $scope.reviewPanel.relationships.push({
+        contacts: '',
+        type: ''
+      });
+    }
+
+    /**
+     * Remove sent Relations
+     *
+     * @param {number} index index of relation to be removed
+     */
+    function removeRelation (index) {
+      $scope.reviewPanel.relationships.splice(index, 1);
+    }
+
+    /**
+     * Call API to get Relationships Types
+     *
+     * @returns {Promise} promise
+     */
+    function getRelationshipsTypes () {
+      return crmApi('RelationshipType', 'get', {
+        sequential: 1,
+        is_active: 1,
+        options: { sort: 'label_a_b', limit: 0 }
+      }).then(function (relationshipTypeData) {
+        return relationshipTypeData;
+      });
+    }
+
+    /**
+     * Prepare list of relationships divided into a to b and b to a
+     *
+     * @param {Array} relationshipTypeData list of relationship types
+     * @returns {Array} list of relationships divided into a to b and b to a
+     */
+    function prepareRelationshipsTypes (relationshipTypeData) {
+      var relationshipTypes = [];
+
+      _.each(relationshipTypeData, function (relationshipType) {
+        relationshipTypes.push({
+          text: relationshipType.label_a_b,
+          id: relationshipType.id + '_a_b'
+        });
+        relationshipTypes.push({
+          text: relationshipType.label_b_a,
+          id: relationshipType.id + '_b_a'
+        });
+      });
+
+      return relationshipTypes;
+    }
 
     /**
      * Open the Popup to Create Review Panels
@@ -42,6 +117,24 @@
     }
 
     /**
+     * Prepare relatiosnhip data before calling API to save
+     *
+     * @returns {Promise} promise
+     */
+    function prepareRelationshipsForSave () {
+      return _.map($scope.reviewPanel.relationships, function (relation) {
+        var isAToB = relation.type.indexOf('a_b') !== -1;
+        var relationshipTypeId = relation.type.substr(0, relation.type.indexOf('_'));
+
+        return {
+          is_a_to_b: isAToB,
+          relationship_type_id: relationshipTypeId,
+          contact_id: getSelect2Value(relation.contacts)
+        };
+      });
+    }
+
+    /**
      * Save Review Panel
      *
      * @returns {Promise} promise
@@ -54,7 +147,10 @@
       var params = {
         title: $scope.reviewPanel.title,
         is_active: $scope.reviewPanel.isEnabled ? '1' : '0',
-        case_type_id: $scope.awardId
+        case_type_id: $scope.awardId,
+        exclude_groups: $scope.reviewPanel.groups.exclude,
+        include_groups: $scope.reviewPanel.groups.include,
+        relationship: prepareRelationshipsForSave()
       };
 
       $scope.submitInProgress = true;
---
+++ ang/civiawards/shared/directives/contacts-group-dropdown.directive.js
@@ -0,0 +1,191 @@
+(function (angular, $, _) {
+  var module = angular.module('civiawards');
+
+  module.directive('civiawardContactsGroupDropdown', function (crmApi) {
+    return {
+      link: civiawardContactsGroupDropdownLink,
+      restrict: 'A',
+      require: 'ngModel'
+    };
+
+    /**
+     * Contacts Group Dropdown Link function
+     *
+     * @param {object} scope scope object of the controller
+     * @param {object} elem directives element
+     * @param {object} attr attributes of the directive element
+     * @param {object} ngModel ng model service
+     */
+    function civiawardContactsGroupDropdownLink (scope, elem, attr, ngModel) {
+      scope.groups = {
+        more: false,
+        results: [{
+          text: 'Include Group',
+          children: []
+        }, {
+          text: 'Exclude Group',
+          children: []
+        }]
+      };
+
+      (function init () {
+        ngModel.$render = refreshUI;
+
+        fetchGroupsData()
+          .then(prepareGroupsDataForSelect2)
+          .then(initSelect2)
+          .then(refreshUI);
+
+        scope.$watchCollection('recips.include', refreshUI);
+        scope.$watchCollection('recips.exclude', refreshUI);
+      }());
+
+      /**
+       * Initialise Select 2
+       */
+      function initSelect2 () {
+        elem.select2({
+          containerCssClass: 'civiaward__contacts-group__container',
+          multiple: true,
+          data: scope.groups,
+          dropdownAutoWidth: true,
+          formatResult: formatItem,
+          formatSelection: formatItem
+        });
+
+        elem.on('select2-selecting', selectEventHandler);
+
+        $(elem).on('select2-removing', removeEventHandler);
+      }
+
+      /**
+       * @param groupsData
+       */
+      function prepareGroupsDataForSelect2 (groupsData) {
+        _.chain(groupsData)
+          .filter(function (group) {
+            return group.extra.is_hidden === '0' && group.extra.is_active === '1';
+          })
+          .each(function (group) {
+            scope.groups.results[0].children.push({
+              id: 'i' + group.id,
+              value: group.id,
+              mode: 'include',
+              text: group.label
+            });
+            scope.groups.results[1].children.push({
+              id: 'e' + group.id,
+              value: group.id,
+              mode: 'exclude',
+              text: group.label
+            });
+          }).value();
+      }
+
+      /**
+       * Fetch Groups data using API
+       *
+       * @returns {Promise} promise
+       */
+      function fetchGroupsData () {
+        return crmApi('Group', 'getlist', {
+          options: { limit: 0 },
+          extra: ['is_hidden', 'is_active']
+        }).then(function (groupsData) {
+          return groupsData.values;
+        });
+      }
+
+      /**
+       * Select event handler for dropdown
+       *
+       * @param {object} e event object
+       */
+      function selectEventHandler (e) {
+        if (e.object.mode === 'exclude') {
+          ngModel.$modelValue.exclude.push(e.object.value);
+          arrayRemove(ngModel.$modelValue.include, e.object.value);
+        } else {
+          ngModel.$modelValue.include.push(e.object.value);
+          arrayRemove(ngModel.$modelValue.exclude, e.object.value);
+        }
+
+        scope.$apply();
+        $(elem).select2('close');
+        e.preventDefault();
+      }
+
+      /**
+       * Remove event handler for dropdown
+       *
+       * @param {object} e event object
+       */
+      function removeEventHandler (e) {
+        arrayRemove(ngModel.$modelValue[e.choice.mode], e.choice.value);
+
+        scope.$parent.$apply();
+
+        e.preventDefault();
+      }
+
+      /**
+       * Show selected values in dropddown
+       */
+      function refreshUI () {
+        scope.recips = ngModel.$modelValue;
+        if (ngModel.$modelValue) {
+          $(elem).select2('val', convertGroupsToValues(ngModel.$modelValue));
+        }
+      }
+
+      /**
+       * Convert selected values to the format suitable for Select2
+       *
+       * @param {object} values selected values
+       * @returns {Array} selected values in format suitable for Select2
+       */
+      function convertGroupsToValues (values) {
+        var r = [];
+
+        _.each(values.include, function (v) {
+          r.push('i' + v);
+        });
+
+        _.each(values.exclude, function (v) {
+          r.push('e' + v);
+        });
+
+        return r;
+      }
+
+      /**
+       * Remove value from sent array
+       *
+       * @param {Array} array array from where value will be removed
+       * @param {number} value value to be removed
+       */
+      function arrayRemove (array, value) {
+        var idx = array.indexOf(value);
+        if (idx >= 0) {
+          array.splice(idx, 1);
+        }
+      }
+
+      /**
+       * Format dropdown item
+       *
+       * @param {object} item item to be formatted
+       * @returns {string} markup to be shown
+       */
+      function formatItem (item) {
+        if (!item.id) {
+          return item.text; // return `text` for optgroup
+        }
+
+        var spanClass = (item.mode === 'exclude') ? 'civiaward__contacts-group__item--exclude' : 'civiaward__contacts-group__item--include';
+
+        return '<i class="crm-i fa-users"></i> <span class="' + spanClass + '">' + item.text + '</span>';
+      }
+    }
+  });
+})(angular, CRM.$, CRM._);
