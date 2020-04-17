0c0b6d3fb8c654d9960ff6d109d5434b901bbfe9
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-04-17 17:19:12 +0100

    ACU-67: Add tests for the two parameters

--- tests/phpunit/CRM/CiviAwards/BAO/AwardReviewPanelTest.php
+++ tests/phpunit/CRM/CiviAwards/BAO/AwardReviewPanelTest.php
@@ -113,4 +113,78 @@ class CRM_CiviAwards_BAO_AwardReviewPanelTest extends BaseHeadlessTest {
 
     AwardReviewPanelFabricator::fabricate($params);
   }
+
+  public function testCreateThrowsExceptionWhenRestrictedStatusIsEmptyAndApplicationStatusIsRestricted() {
+    $params = [
+      'visibility_settings' => [
+        'application_status' => [1,3],
+        'anonymize_application' => 0,
+        'is_application_status_restricted' => 1
+      ]
+    ];
+
+    $this->setExpectedException(
+      'Exception',
+      'Visibility Settings: restricted_application_status is required'
+    );
+
+    AwardReviewPanelFabricator::fabricate($params);
+  }
+
+  public function testCreateThrowsExceptionWhenIsApplicationStatusRestrictedNotPresent() {
+    $params = [
+      'visibility_settings' => [
+        'application_status' => [1,3],
+        'anonymize_application' => 0,
+      ]
+    ];
+
+    $this->setExpectedException(
+      'Exception',
+      'Visibility Settings: is_application_status_restricted is required'
+    );
+
+    AwardReviewPanelFabricator::fabricate($params);
+  }
+
+  public function testCreateThrowsExceptionWhenRestrictedStatusVisibilitySettingContainsNonInteger() {
+    $params = [
+      'visibility_settings' => [
+        'application_status' => [1,3],
+        'anonymize_application' => 0,
+        'is_application_status_restricted' => 1,
+        'restricted_application_status' => [1,3,'Sample'],
+      ]
+    ];
+
+    $this->setExpectedException(
+      'Exception',
+      'Visibility Settings: One of the values of restricted_application_status is not in a valid format'
+    );
+
+    AwardReviewPanelFabricator::fabricate($params);
+  }
+
+  public function testCreateIsSuccessFulWhenAllPArametersAreInExpectedFormat() {
+    $params = [
+      'visibility_settings' => [
+        'application_status' => [1,3],
+        'anonymize_application' => 0,
+        'is_application_status_restricted' => 1,
+        'restricted_application_status' => [1,3],
+      ],
+      'contact_settings' => [
+        'exclude_groups' => [1, 3],
+        'include_groups' => [5],
+        'relationship' => [
+          'contact_id' => [1, 3],
+          'relationship_type_id' => 1,
+          'is_a_to_b' => 1
+        ]
+      ]
+    ];
+
+    $awardPanel = AwardReviewPanelFabricator::fabricate($params);
+    $this->assertNotEmpty($awardPanel->id);
+  }
 }
