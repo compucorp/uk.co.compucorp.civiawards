2f91e1fd00288d60e93b60654991ddecb45a97bf
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-04-17 17:19:52 +0100

    ACU-67: Code spacing fix

--- CRM/CiviAwards/BAO/AwardReviewPanel.php
+++ CRM/CiviAwards/BAO/AwardReviewPanel.php
@@ -179,19 +179,19 @@ class CRM_CiviAwards_BAO_AwardReviewPanel extends CRM_CiviAwards_DAO_AwardReview
         continue;
       }
       if (!empty($setting['is_required']) && !isset($params[$fieldName])) {
-        throw new Exception(ts("{$settingName} :  {$fieldName} is required"));
+        throw new Exception(ts("{$settingName}: {$fieldName} is required"));
       }
 
       if (!empty($setting['is_array'])) {
         if (!is_array($params[$fieldName])) {
-          throw new Exception(ts("{$settingName} :  {$fieldName} should be an array"));
+          throw new Exception(ts("{$settingName}: {$fieldName} should be an array"));
         }
 
         if (!empty($setting['validate_filter'])) {
           $options = isset($setting['options']) ? ['options' => $setting['options']] : NULL;
           foreach ($params[$fieldName] as $value) {
             if (filter_var($value, $setting['validate_filter'], $options) === FALSE) {
-              throw new Exception("{$settingName} :  One of the values of {$fieldName} is not in a valid format");
+              throw new Exception("{$settingName}: One of the values of {$fieldName} is not in a valid format");
             }
           }
         }
@@ -200,7 +200,7 @@ class CRM_CiviAwards_BAO_AwardReviewPanel extends CRM_CiviAwards_DAO_AwardReview
       if (empty($setting['is_array']) && !empty($setting['validate_filter'])) {
         $options = isset($setting['options']) ? ['options' => $setting['options']] : NULL;
         if (filter_var($params[$fieldName], $setting['validate_filter'], $options) === FALSE) {
-          throw new Exception("{$settingName} :  {$fieldName} is not in a valid format");
+          throw new Exception("{$settingName}: {$fieldName} is not in a valid format");
         }
       }
     }
