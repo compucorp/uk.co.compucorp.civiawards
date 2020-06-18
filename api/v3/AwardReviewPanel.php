4494393e7a87022c923c2e8c50ac8535bddabb4a
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-06-18 19:12:14 +0100

    RSE-1139: Add getReviewAcess API.

--- api/v3/AwardReviewPanel.php
+++ api/v3/AwardReviewPanel.php
@@ -49,6 +49,49 @@ function civicrm_api3_award_review_panel_getcontactaccess(array $params) {
 }
 
 /**
+ * AwardReviewPanel.getReviewAccess API specification.
+ *
+ * @param array $spec
+ *   Description of fields supported by this API call.
+ */
+function _civicrm_api3_award_review_panel_getreviewaccess_spec(array &$spec) {
+  $spec['contact_id'] = [
+    'title' => 'Contact ID',
+    'description' => 'Award Panel Contact ID',
+    'type' => CRM_Utils_Type::T_INT,
+    'api.required' => 1,
+    'FKClassName' => 'CRM_Contact_DAO_Contact',
+    'FKApiName' => 'Contact',
+  ];
+
+  $spec['application_id'] = [
+    'title' => 'Application ID',
+    'description' => 'Application ID',
+    'type' => CRM_Utils_Type::T_INT,
+    'api.required' => 1,
+    'FKClassName' => 'CRM_Case_BAO_Case',
+    'FKApiName' => 'Case',
+  ];
+}
+
+/**
+ * AwardReviewPanel.getReviewAccess API.
+ *
+ * @param array $params
+ *   Parameters.
+ *
+ * @return array
+ *   API result descriptor.
+ */
+function civicrm_api3_award_review_panel_getreviewaccess(array $params) {
+  $contactAccessService = new CRM_CiviAwards_Service_AwardApplicationContactAccess();
+  $awardPanelContact = new CRM_CiviAwards_Service_AwardPanelContact();
+  $contactAccess = $contactAccessService->getReviewAccess($params['contact_id'], $params['application_id'], $awardPanelContact);
+
+  return civicrm_api3_create_success($contactAccess);
+}
+
+/**
  * AwardReviewPanel.create API.
  *
  * @param array $params
