ee5755458a7ec8a625acebc78a1b0ace32eeb72a
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-07-06 16:52:04 +0100

    RSE-1209: Add Applicant Management Word Replacement Functionality

--- CRM/CiviAwards/Setup/AddApplicationManagementWordReplacement.php
+++ CRM/CiviAwards/Setup/AddApplicationManagementWordReplacement.php
@@ -5,18 +5,18 @@ use CRM_CiviAwards_Helper_CaseTypeCategory as CaseTypeCategoryHelper;
 /**
  * Class CRM_CiviAwards_Setup_AddAwardsCategoryWordReplacement.
  */
-class CRM_CiviAwards_Setup_AddAwardsCategoryWordReplacement {
+class CRM_CiviAwards_Setup_AddApplicationManagementWordReplacement {
 
   /**
    * Creates the Awards word replacement option value.
    */
   public function apply() {
-    $optionName = CaseTypeCategoryHelper::AWARDS_CASE_TYPE_CATEGORY_NAME . "_word_replacement";
+    $optionName = CaseTypeCategoryHelper::APPLICATION_MANAGEMENT_NAME . "_word_replacement";
     CRM_Core_BAO_OptionValue::ensureOptionValueExists([
       'option_group_id' => 'case_type_category_word_replacement_class',
       'name' => $optionName,
       'label' => $optionName,
-      'value' => 'CRM_CiviAwards_WordReplacement_AwardsCategory',
+      'value' => 'CRM_CiviAwards_WordReplacement_ApplicantManagement',
       'is_default' => 1,
       'is_active' => TRUE,
       'is_reserved' => TRUE,
