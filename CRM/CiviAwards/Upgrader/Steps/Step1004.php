9b35906251243413aa70312e291a620c3880a6ea
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-07-09 13:43:01 +0100

    RSE-647: Add missing Upgrader. Bump Upgrader Number

---
+++ CRM/CiviAwards/Upgrader/Steps/Step1004.php
@@ -0,0 +1,23 @@
+<?php
+
+use CRM_CiviAwards_Setup_AddAwardCaseTypeCustomGroupSupport as AddAwardCaseTypeCustomGroupSupport;
+
+/**
+ * Upgrader that adds custom group support for Award Case Type.
+ */
+class CRM_CiviAwards_Upgrader_Steps_Step1004 {
+
+  /**
+   * Adds Custom Group support for Award Case Type.
+   *
+   * @return bool
+   *   returns value.
+   */
+  public function apply() {
+    $step = new AddAwardCaseTypeCustomGroupSupport();
+    $step->apply();
+
+    return TRUE;
+  }
+
+}
