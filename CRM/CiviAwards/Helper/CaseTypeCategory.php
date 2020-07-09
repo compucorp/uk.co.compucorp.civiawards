781d57678b487bd3f463caa792765ab4f32a7d46
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-07-09 17:05:52 +0100

    RSE-1200: Update function to check for Award Instance case types. Use in form class

--- CRM/CiviAwards/Helper/CaseTypeCategory.php
+++ CRM/CiviAwards/Helper/CaseTypeCategory.php
@@ -9,15 +9,24 @@ class CRM_CiviAwards_Helper_CaseTypeCategory {
   const APPLICATION_MANAGEMENT_NAME = 'applicant_management';
 
   /**
-   * Returns the case types for the awards category.
+   * Returns the case types for the applicant management instance.
    *
    * @return array
    *   Array of Case Types indexed by Id.
    */
-  public static function getAwardCaseTypes() {
+  public static function getApplicantManagementCaseTypes() {
+    $instance = civicrm_api3('CaseCategoryInstance', 'get', [
+      'instance_id' => self::APPLICATION_MANAGEMENT_NAME,
+    ]);
+
+    if (empty($instance['values'])) {
+      return [];
+    }
+
+    $applicantManagementCategories = array_column($instance['values'], 'category_id');
     $result = civicrm_api3('CaseType', 'get', [
       'return' => ['title', 'id'],
-      'case_type_category' => self::AWARDS_CASE_TYPE_CATEGORY_NAME,
+      'case_type_category' => ['IN' => $applicantManagementCategories],
     ]);
 
     return array_column($result['values'], 'title', 'id');
