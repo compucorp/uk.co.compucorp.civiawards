f4beb254e891d010c26667e0e92643cdec590818
René Olivo <rene@compucorp.co.uk>
2021-02-12 21:02:47 +0000

    ACU-500: Fix spec sentences
    The sentences must play along with the 'it' statements.

--- ang/test/civiawards-payments-tab/services/awards-payment-activity-status.service.spec.js
+++ ang/test/civiawards-payments-tab/services/awards-payment-activity-status.service.spec.js
@@ -10,28 +10,28 @@ describe('AwardsPaymentActivityStatus', () => {
   }));
 
   describe('when payment is complete', () => {
-    it('delete action is not visible', () => {
+    it('hides the delete action', () => {
       expect(AwardsPaymentActivityStatus.isDeleteVisible({ status_name: 'paid_complete' }))
         .toBe(false);
     });
   });
 
   describe('when payment has failed', () => {
-    it('delete action is not visible', () => {
+    it('hides the delete action', () => {
       expect(AwardsPaymentActivityStatus.isDeleteVisible({ status_name: 'failed_incomplete' }))
         .toBe(false);
     });
   });
 
   describe('when payment has been exported', () => {
-    it('delete action is not visible', () => {
+    it('hides the delete action', () => {
       expect(AwardsPaymentActivityStatus.isDeleteVisible({ status_name: 'exported_complete' }))
         .toBe(false);
     });
   });
 
   describe('when payment is not complete, failed or exported', () => {
-    it('delete action is visible', () => {
+    it('hides the delete action', () => {
       expect(AwardsPaymentActivityStatus.isDeleteVisible({ status_name: 'applied_for_incomplete' }))
         .toBe(true);
     });
