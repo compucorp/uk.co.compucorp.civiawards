011e20b1293211c1749c5a1345ba6084234a3fee
Tano Rojas <lisandro@compucorp.co.uk>
2021-04-07 19:56:16 +0700

    CPS-202: Use Route classes for ApplicantManagement menu

--- CRM/CiviAwards/Service/ApplicantManagementMenu.php
+++ CRM/CiviAwards/Service/ApplicantManagementMenu.php
@@ -1,54 +1,28 @@
 <?php
 
+use CRM_Civicase_Service_CaseMenu_RouteFactory as RouteFactory;
+
 /**
  * Applicant Management Menu class.
  */
 class CRM_CiviAwards_Service_ApplicantManagementMenu extends CRM_Civicase_Service_CaseCategoryMenu {
 
   /**
-   * Creates the Sub Menus for the Applicant Management Instance.
-   *
-   * @param string $caseTypeCategoryName
-   *   Case category name.
-   * @param array $permissions
-   *   Permissions.
-   * @param int $caseCategoryMenuId
-   *   Menu ID.
+   * {@inheritDoc}
    */
-  protected function createCaseCategorySubmenus($caseTypeCategoryName, array $permissions, $caseCategoryMenuId) {
-    $labelForMenu = ucfirst(strtolower($caseTypeCategoryName));
-
-    $submenus = [
-      [
-        'label' => ts('Dashboard'),
-        'name' => "{$caseTypeCategoryName}_dashboard",
-        'url' => "/civicrm/case/a/?case_type_category={$caseTypeCategoryName}#/case?case_type_category={$caseTypeCategoryName}",
-        'permission' => "{$permissions['ACCESS_MY_CASE_CATEGORY_AND_ACTIVITIES']['name']},{$permissions['ACCESS_CASE_CATEGORY_AND_ACTIVITIES']['name']}",
-        'permission_operator' => 'OR',
-      ],
-      [
-        'label' => ts('Manage Applications'),
-        'name' => "manage_{$caseTypeCategoryName}_applications",
-        'url' => 'civicrm/case/a/?case_type_category=' . $caseTypeCategoryName . '#/case/list?cf={"case_type_category":"' . $caseTypeCategoryName . '"}',
-        'permission' => "{$permissions['ACCESS_MY_CASE_CATEGORY_AND_ACTIVITIES']['name']},{$permissions['ACCESS_CASE_CATEGORY_AND_ACTIVITIES']['name']}",
-        'permission_operator' => 'OR',
-        'has_separator' => 1,
-      ],
-      [
-        'label' => ts("Manage " . $labelForMenu),
-        'name' => "manage_{$caseTypeCategoryName}_workflows",
-        'url' => 'civicrm/workflow/a?case_type_category=' . $caseTypeCategoryName . '#/list',
-        'permission' => "{$permissions['ADMINISTER_CASE_CATEGORY']['name']}, administer CiviCRM",
-        'permission_operator' => 'OR',
-      ],
+  public function getSubRoutes(string $caseTypeCategoryName) {
+    return [
+      RouteFactory::create('dashboard', $caseTypeCategoryName),
+      RouteFactory::create('all', $caseTypeCategoryName)
+        ->setOverwrittenProperties([
+          'label' => ts('Manage Applications'),
+          'name' => "manage_{$caseTypeCategoryName}_applications",
+        ]),
+      RouteFactory::create('manage_workflows', $caseTypeCategoryName)
+        ->setOverwrittenProperties([
+          'label' => ts("Manage " . $caseTypeCategoryName),
+        ]),
     ];
-
-    foreach ($submenus as $i => $item) {
-      $item['weight'] = $i;
-      $item['parent_id'] = $caseCategoryMenuId;
-      $item['is_active'] = 1;
-      civicrm_api3('Navigation', 'create', $item);
-    }
   }
 
 }
