8e351634d4819a06d082ce06858eeb6df3dfa1cd
Tano Rojas <lisandro@compucorp.co.uk>
2021-04-07 19:56:52 +0700

    CPS-202: Add upgrader for menu changes

---
+++ CRM/CiviAwards/Upgrader/Steps/Step1011.php
@@ -0,0 +1,26 @@
+<?php
+
+use CRM_CiviAwards_Service_ApplicantManagementMenu as ApplicantManagementMenu;
+
+/**
+ * Update menus with new URL.
+ */
+class CRM_CiviAwards_Upgrader_Steps_Step1011 {
+
+  /**
+   * Runs the upgrader changes.
+   *
+   * @return bool
+   *   Return value in boolean.
+   */
+  public function apply() {
+    (new ApplicantManagementMenu())->updateSubmenus(
+      CRM_CiviAwards_Helper_CaseTypeCategory::AWARDS_CASE_TYPE_CATEGORY_NAME
+    );
+
+    CRM_Core_BAO_Navigation::resetNavigation();
+
+    return TRUE;
+  }
+
+}
