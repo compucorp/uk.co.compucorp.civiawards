f8033a26ed9b230efd01912a3c7b6e7c896ea291
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2022-09-27 15:57:56 +0100

    MAE-893: Supress reflection depreciation error

--- tests/phpunit/BaseHeadlessTest.php
+++ tests/phpunit/BaseHeadlessTest.php
@@ -1,16 +1,59 @@
 <?php
 
+use Civi\Test;
 use Civi\Test\HeadlessInterface;
 use Civi\Test\TransactionalInterface;
 
-abstract class BaseHeadlessTest extends PHPUnit_Framework_TestCase implements
-  HeadlessInterface, TransactionalInterface {
+/**
+ * Base test class.
+ */
+abstract class BaseHeadlessTest extends PHPUnit_Framework_TestCase implements HeadlessInterface, TransactionalInterface {
 
+  /**
+   * {@inheritDoc}
+   */
   public function setUpHeadless() {
-    return \Civi\Test::headless()
+    return Test::headless()
       ->install('uk.co.compucorp.civicase')
       ->installMe(__DIR__)
       ->apply();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getMockBuilder($className) {
+    $mockBuilder = (new class($this, $className) extends PHPUnit_Framework_MockObject_MockBuilder {
+
+      /**
+       * {@inheritDoc}
+       */
+      public function getMock() {
+        static::setSupressedErrorHandler();
+
+        try {
+          return parent::getMock();
+        } finally {
+          restore_error_handler();
+        }
+      }
+
+      /**
+       * Supress depreciation warnings.
+       */
+      public static function setSupressedErrorHandler() {
+        $previousHandler = set_error_handler(function ($code, $description, $file = NULL, $line = NULL, $context = NULL) use (&$previousHandler) {
+          if ($code & E_DEPRECATED) {
+              return TRUE;
+          }
+
+            return $previousHandler($code, $description, $file, $line, $context);
+        });
+      }
+
+    });
+
+    return $mockBuilder;
   }
 
 }
